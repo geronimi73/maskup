1d90b8b21526f3405a70ef3dc25eec4bf5a291c1
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2025-07-10 10:56:17 +0200

    add logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 
 import { Toaster } from "@/components/ui/toaster"
+import Image from 'next/image';
 
 export const metadata = {
   title: "Maskup - ML Dataset Annotation Tool",
@@ -15,14 +16,22 @@ export default function RootLayout({ children }) {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center py-4">
               <div className="flex items-center space-x-3">
-                <div className="w-10 h-10 bg-gradient-to-br from-blue-500 to-purple-600 rounded-lg flex items-center justify-center">
+                <div>
+                  <Image
+                    src="/logo.png" // Path to the image in the public folder
+                    // alt="Description of the image" // Alt text for accessibility
+                    width={50} // Desired width in pixels
+                    height={50} // Desired height in pixels
+                  />                
+                </div>
+{/*                <div className="w-10 h-10 bg-gradient-to-br from-blue-500 to-purple-600 rounded-lg flex items-center justify-center">
                   <svg viewBox="0 0 24 24" className="w-6 h-6 text-white" fill="currentColor">
                     <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.94-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z" />
                   </svg>
-                </div>
-                <h1 className="text-2xl font-bold text-gray-900">Maskup</h1>
+                </div>*/}
+                <h1 className="text-2xl font-bold text-gray-900">MaskUp</h1>
               </div>
-              <p className="text-sm text-gray-600">ML Dataset Annotation Tool</p>
+              <p className="text-sm text-gray-600">Image Dataset Annotation Tool</p>
             </div>
           </div>
         </header>
